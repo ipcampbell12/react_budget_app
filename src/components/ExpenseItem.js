d2eb5ac8e95edcf0f047d171405c0d93d994b1fd
ipcampbell12 <ipcampbell12@gmail.com>
2022-12-07 16:57:18 -0800

    currency format for items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { numberFormatter } from '../App';
 
 function ExpenseItem({ item, total, onDelete }) {
     return (
@@ -6,7 +7,7 @@ function ExpenseItem({ item, total, onDelete }) {
         <tr className="table-danger">
             <td>{item.description}</td>
             <td>{item.category}</td>
-            <td>{item.value}</td>
+            <td>{numberFormatter.format(item.value)}</td>
             <td>{total === 0 ? 0 : Math.round((parseFloat(item.value) / total) * 100) + '%'}</td>
             <td><button className='btn btn-danger' onClick={() => onDelete(item.id)}> X </button> </td>
         </tr>
--- src/components/IncomItem.js
+++ src/components/IncomItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { numberFormatter } from '../App';
 
 function IncomeItem({ item, onDelete }) {
     return (
         <tr className="table-success">
             <td>{item.description}</td>
             <td>{item.category}</td>
-            <td>{item.value}</td>
+            <td>{numberFormatter.format(item.value)}</td>
             <td><button className='btn btn-danger' onClick={() => onDelete(item.id)}> X </button> </td>
         </tr>
     );
